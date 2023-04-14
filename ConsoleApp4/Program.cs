09260b965c349feacb4920b3afdf212c3beacf90
Kluczleks <kluczleks.twitch@gmail.com>
2023-04-14 10:22:08 +0200

    wersja 1

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//zdefiniuj string do zaszyfrowania
+string clearText = "Ala ma kota, kot ma Ale, a sierotka ma rysia";
+//zamieniamy na wielkie litery 
+clearText = clearText.ToUpper();
+
+//iteruj przez cały tekst litera po literze 
+//deklarujemy string wynikowy 
+string encryptedText = " ";
+foreach (char c in clearText)
+{
+    //wyciagamy kod asci danej litery
+    // w języku c# rzutowanie jawne z char z int powodyhe pobranie wartosci ascii
+    int asciiCode = (int)c;
+   //dodajemy wartość klucza 
+    asciiCode += 3;
+    //zamieniamy kod z powrotem na znak 
+    char encryptedChar = (char)asciiCode;
+    //dopisujemy do szyfru
+    encryptedText += encryptedChar;
+}
+
+Console.WriteLine("Zaszyfrowany tekst: " + encryptedText);
